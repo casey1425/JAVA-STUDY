85b5dbb749a1bebb99c423bc3f6ba47e8be4fd91
casey1425 <121206446+casey1425@users.noreply.github.com>
2023-12-26 22:41:11 +0900

    1일차

---
+++ day_01/src/day_01/test_01.java
@@ -0,0 +1,12 @@
+package day_01;
+
+public class test_01 {
+	public static void main(String[] args) {
+		// ctrl + spacebar : main method 자동완성
+		System.out.println("1일차");
+		//syso + ctrl + spacebar : output method 자동완성
+//		주석 단축키 : 한줄주석 -> ctrl + /
+//		여러줄 주석 : ctrl + shift + / (설정)
+//				ctrl + shift + \ (해제)
+	}
+}
