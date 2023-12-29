05518ba0e219f13318a14f6bff4a371b25bd31e6
casey1425 <leedh1425@naver.com>
2023-12-30 00:39:14 +0900

    edit error files

--- Day_04/Day_04_practice_15.java
+++ Day_04/Day_04_practice_15.java
@@ -1,5 +1,27 @@
 package Day_04;
 
 public class Day_04_practice_15 {
-
+//	논리연산자 &&, ||, ^
+	public static void main(String[] args) {
+		boolean b1=true;
+		boolean b2=false;
+		boolean b3=true;
+		boolean b4=false;
+		
+		System.out.println("-----논리곱-----");//논리곱은 둘다 true일때만 true 나머지는 다 false
+		System.out.println(b1&&b2);//하나가 false이므로 false
+		System.out.println(b1&&b3);//둘다 true이므로 true
+		System.out.println(b2&&b4);//둘다 false이므로 false
+		System.out.println();
+		
+		System.out.println("-----논리합-----");//논리합은 둘다 false일때만 false 나머지는 다 true
+		System.out.println(b1||b2);//b1이 true이므로 true
+		System.out.println(b1||b3);//둘다 true이므로 true
+		System.out.println(b2||b4);//둘다 false이므로 false
+		System.out.println();
+		
+		System.out.println("-----배타적 논리합-----");//둘이 같으면 false, 다르면 true
+		System.out.println(b1^b2);//둘이 다르므로 true
+		System.out.println(b1^b3);//둘이 같으므로 false
+	}
 }
